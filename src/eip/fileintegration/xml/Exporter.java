9a484e61df4e91290153d6e70d8971575ad33138
Ernesto <ernesto.exposito@gmail.com>
2020-04-06 23:04:46 +0200

    EIP file integration design pattern based on XML documents

---
+++ src/eip/fileintegration/xml/Exporter.java
@@ -0,0 +1,28 @@
+package eip.fileintegration.xml;
+
+
+
+import java.io.IOException;
+
+/*
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Exporter {
+
+    public Exporter(String filename) throws IOException {
+        
+    }
+
+    public void exportObject(Object data) {
+        
+    }
+    
+    public void close()
+    {
+        
+    }
+
+}
+
---
+++ src/eip/fileintegration/xml/Importer.java
@@ -0,0 +1,27 @@
+package eip.fileintegration.xml;
+
+
+
+import java.io.IOException;
+
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Importer {
+
+    public Importer(String filename) throws IOException {
+       
+    }
+
+    public Object importObject() {
+        
+        return null;
+    }
+    
+     public void close()
+    {
+        
+        
+    }
+}
